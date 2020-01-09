7cdf050c9a3e99a2dd2a5008203a2a43b081bb97
fadhlanfm <fafadhlanriz@gmail.com>
2020-01-09 18:01:01 +0700

    elemen input harus lebih dari 2

--- exercise-11/tentukanDeretAritmatika.js
+++ exercise-11/tentukanDeretAritmatika.js
@@ -1,16 +1,20 @@
 function tentukanDeretAritmatika(arr) {
   // you can only write your code here!
-  var perselisihan = [];
-  for(i=0;i<arr.length-1;i++){
-      perselisihan.push(arr[i+1]-arr[i]);
+  if(arr.length<=2){
+      return 'error';
+  }else{
+    var perselisihan = [];
+    for(i=0;i<arr.length-1;i++){
+        perselisihan.push(arr[i+1]-arr[i]);
+    }
+    
+    for(i=0;i<perselisihan.length-1;i++){
+        if(perselisihan[i]!=perselisihan[i+1]){
+            return false;
+        }
+    }
+    return true;   
   }
-  
-  for(i=0;i<perselisihan.length-1;i++){
-      if(perselisihan[i]!=perselisihan[i+1]){
-        return false;
-      }
-  }
-  return true;
 }
 
 // TEST CASES
@@ -19,3 +23,7 @@ console.log(tentukanDeretAritmatika([2, 4, 6, 12, 24])); // false
 console.log(tentukanDeretAritmatika([2, 4, 6, 8])); // true
 console.log(tentukanDeretAritmatika([2, 6, 18, 54])); // false
 console.log(tentukanDeretAritmatika([1, 2, 3, 4, 7, 9])); // false
+console.log(tentukanDeretAritmatika([1, 2, 3])); // true
+console.log(tentukanDeretAritmatika([1, 2])); // error
+console.log(tentukanDeretAritmatika([1])); // error
+console.log(tentukanDeretAritmatika([])); // error
