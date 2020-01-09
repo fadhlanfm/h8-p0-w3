3dc469736049f407e4ec6652cb8f3b055a576d89
fadhlanfm <fafadhlanriz@gmail.com>
2020-01-09 15:45:22 +0700

    first commit

---
+++ exercise-8/pasanganTerbesar.js
@@ -0,0 +1,21 @@
+function pasanganTerbesar(num) {
+    // you can only write your code here!
+    var stringyNum = num.toString();
+    var output = stringyNum[0]+stringyNum[1];
+
+    for(i=0;i<stringyNum.length-1;i++){
+        if(output>=stringyNum[i]+stringyNum[i+1]){
+            output = output;
+        }else{
+            output = stringyNum[i]+stringyNum[i+1];   
+        }
+    }
+    return output;
+  }
+  
+  // TEST CASES
+  console.log(pasanganTerbesar(641573)); // 73
+  console.log(pasanganTerbesar(12783456)); // 83
+  console.log(pasanganTerbesar(910233)); // 91
+  console.log(pasanganTerbesar(71856421)); // 85
+  console.log(pasanganTerbesar(79918293)); // 99
